696892fad0bce764f68a6ce217b73e6e93464fed
Edwin Santana <edwsan2001edwsan@gmail.com>
2023-09-12 21:53:37 -0500

    first commit

---
+++ src/comment.php
@@ -0,0 +1,8 @@
+<?php 
+
+/* 
+ For that this to work, you need add a new host in your local machine for example in windows: C:\Windows\System32\drivers\etc\hosts.
+ and add 127.0.0.1  mydomain.com .....
+*/
+
+?>    
---
+++ src/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Docker-PHP</title>
+</head>
+<body>
+<?php echo phpinfo(); ?>    
+
+</body>
+</html>
